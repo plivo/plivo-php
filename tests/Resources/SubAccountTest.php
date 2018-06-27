4e78a1d213e4ef55b004053635da1cc552bf76bf
Nixon Samuel <nixonsamueljnf@gmail.com>
2018-06-27 16:22:18 +0530

    SUP-83 :: fixed subaccount create test case assertion conditions

--- tests/Resources/SubAccountTest.php
+++ tests/Resources/SubAccountTest.php
@@ -33,9 +33,9 @@ class SubAccountTest extends BaseTestCase
 
         self::assertNotNull($actual);
 
-        self::assertEquals($actual->api_id, "97c8d1de-3f08-11e7-b6f4-061564b78b75");
-        self::assertEquals($actual->auth_id, "SANDLHYZBIZMU4ZDEXNM");
-        self::assertEquals($actual->auth_token, "MTMzZTZjNzdiNDVmY2VhZDQyNTUwYWVjNzI2OThk");
+        self::assertEquals($actual->apiId, "97c8d1de-3f08-11e7-b6f4-061564b78b75");
+        self::assertEquals($actual->authId, "SANDLHYZBIZMU4ZDEXNM");
+        self::assertEquals($actual->authToken, "MTMzZTZjNzdiNDVmY2VhZDQyNTUwYWVjNzI2OThk");
         self::assertEquals($actual->message, "created");
     }
 
