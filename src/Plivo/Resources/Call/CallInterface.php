9ef125ce677def4b3f5864203d8c625aa71ec464
Kritarth <kritarth@plivo.com>
2019-06-03 18:25:34 +0530

    add invalid_numbers in bulk call api response

--- src/Plivo/Resources/Call/CallInterface.php
+++ src/Plivo/Resources/Call/CallInterface.php
@@ -100,6 +100,7 @@ class CallInterface extends ResourceInterface
                 $responseContents['api_id'],
                 $responseContents['message'],
                 $responseContents['request_uuid'],
+                $responseContents['invalid_numbers'],
                 $response->getStatusCode()
             );
         } else {
@@ -112,7 +113,7 @@ class CallInterface extends ResourceInterface
 
             );
         }
-        
+
     }
 
     /**
