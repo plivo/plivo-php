24be2b79d64383c55b47349bd8d4d3c3fc564a1e
Kritarth <kritarth@plivo.com>
2020-05-06 09:22:32 +0530

    Update JWT.php

--- src/Examples/JWT.php
+++ src/Examples/JWT.php
@@ -3,11 +3,13 @@ require 'vendor/autoload.php';
 use Plivo\Util\AccessToken;
 
 // using validFrom and lifetime
-$acctkn = new AccessToken("{authId}", "{authToken}", "{endpointUsername}", gmdate('U'), 3600, null, "{uid}");
+$acctkn = new AccessToken("{authId}", "{authToken}", "{endpointUsername}", gmdate('U'), 3600, null);
+// grants(incoming:false, outgoing:true)
 $acctkn->addVoiceGrants(false, true);
 echo $acctkn->toJwt() . "\n";
 
-// using validFrom and validTill
+// using validFrom and validTill, with custom uid
 $acctkn = new AccessToken("{authId}", "{authToken}", "{endpointUsername}", gmdate('U'), null, gmdate('U', mktime(23, 59, 59, 4, 29, 2020)), "{uid}");
-$acctkn->addVoiceGrants(false, true);
+// grants(incoming:true, outgoing:false)
+$acctkn->addVoiceGrants(true, false);
 echo $acctkn->toJwt() . "\n";
