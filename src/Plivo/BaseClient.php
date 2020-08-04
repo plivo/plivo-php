8fbae80b2c41453380db7c2b48dbe2ec7f5375fd
Koushik-Ayila <koushik.ayila@plivo.com>
2020-08-04 18:09:18 +0530

    Modified Default Request Timeout to be 20 sec

--- src/Plivo/BaseClient.php
+++ src/Plivo/BaseClient.php
@@ -27,7 +27,7 @@ class BaseClient
     /**
      * @const Default timeout for request
      */
-    const DEFAULT_REQUEST_TIMEOUT = 5;
+    const DEFAULT_REQUEST_TIMEOUT = 20;
 
     /**
      * @var int|null Request timeout
