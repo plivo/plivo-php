eae60e182c2dc9238931eb6b0c26c0149562a832
huzaif <huzaif@plivo.com>
2020-12-14 14:41:21 +0530

    catch exception - message dst fatal error

--- src/Plivo/Resources/Message/MessageInterface.php
+++ src/Plivo/Resources/Message/MessageInterface.php
@@ -144,9 +144,13 @@ class MessageInterface extends ResourceInterface
      * @throws PlivoValidationException,PlivoResponseException
      */
 
-    public function create($src, array $dst, $text=null,
-                           array $optionalArgs = [], $powerpackUUID = null)
+    public function create($src, $dst, $text=null,array $optionalArgs = [], $powerpackUUID = null)
     {
+        if (!is_array($dst))
+        {
+            throw new PlivoValidationException("Destination parameter must be of the type array");
+        }        
+        
         $mandatoryArgs = [
             'dst' => implode('<', $dst),
         ];
