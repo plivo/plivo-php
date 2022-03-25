6f8aaadcce25954f9b6ca58227d70fe0bf0cbf0a
manjunath-plivo <85923934+manjunath-plivo@users.noreply.github.com>
2022-03-25 08:36:27 +0530

    change version

--- src/Plivo/Version.php
+++ src/Plivo/Version.php
@@ -20,7 +20,7 @@ class Version
     /**
      * @const int PHP helper library minor version number
      */
-    const MINOR = 27;
+    const MINOR = 28;
 
     /**
      * @const int PHP helper library patch number
