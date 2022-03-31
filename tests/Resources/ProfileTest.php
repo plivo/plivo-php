2a5e5cf9eda6b70781d7a11d2eecdb1b44fcb129
narayana <narayana@plivo.com>
2022-03-31 09:23:08 +0530

    profile api support

--- tests/Resources/ProfileTest.php
+++ tests/Resources/ProfileTest.php
@@ -16,87 +16,52 @@ use Plivo\Tests\BaseTestCase;
  */
 class ProfileTest extends BaseTestCase {
 
-    public function testCampaignCreate()
+    function testProfileGet()
     {
         $request = new PlivoRequest(
-            'POST',
-            'Account/MAXXXXXXXXXXXXXXXXXX/Profile/',
-            [
-            
-                    "profile_alias" => "vishnu109",
-                    "profile_type"=> "SECONDARY",
-                    "customer_type"=> "RESELLER",
-                    "entity_type"=> "PRIVATE_PROFIT",
-                    "company_name"=> "ABC Inc.",
-                    "ein" => "111111111",
-                    "vertical" => "PROFESSIONAL",
-                    "ein_issuing_country" => "US",
-                    "address" => {
-                        "street" =>"123",
-                        "city" => "New York",
-                        "state" => "NY",
-                        "postal_code" => "10001",
-                        "country" => "IN"
-                    },
-                    "stock_symbol" => "ABC",
-                    "stock_exchange" => "NASDAQ",
-                    "alt_business_id_type" => "NONE",
-                    "authorized_contact" => {
-                        "first_name" => "John",
-                        "last_name" => "Doe",
-                        "email" => "johndoe.com",
-                        "title" => "Doe",
-                        "seniority" => "admin"
-                    }
-                
-                ]);
-        $body = file_get_contents(__DIR__ . '/../Mocks/profileCreateResponse.json');
+            'GET',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Profile/06ecae31-4bf8-40b9-ac62-e902418e9935/', []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/profileGetResponse.json');
 
         $this->mock(new PlivoResponse($request,200, $body));
 
-        // $actual = $this->client->profile->create("B8OD95Z", "campaign name sssample","INSURANCE", "MIXED", [
-        //     "CUSTOMER_CARE",
-        //     "2FA"
-        // ], "sample description text", False, False, False, False, True, True, True, "test 1", "test 2");
+        $actual = $this->client->profile->get('06ecae31-4bf8-40b9-ac62-e902418e9935');
 
-        // $this->assertRequest($request);
+        $this->assertRequest($request);
 
-        // self::assertNotNull($actual);
+        self::assertNotNull($actual);
     }
 
-    // public function testGetCampaign()
-    // {
-    //     $campaignID = "CMPT4EP";
-    //     $request = new PlivoRequest(
-    //         'GET',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/10dlc/Campaign/'.$campaignID.'/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/campaignGetResponse.json');
-
-    //     $this->mock(new PlivoResponse($request,200, $body));
-
-    //     $actual = $this->client->campaign->get($campaignID);
-    //     self::assertNotNull($actual);
-    //     $this->assertRequest($request);
-
-    // }
-   
-    
-    // function testCampaignList()
-    // {
-    //     $request = new PlivoRequest(
-    //         'Get',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/10dlc/Campaign/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/campaignListResponse.json');
+    function testProfileList()
+    {
+        $request = new PlivoRequest(
+            'Get',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Profile/',
+            []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/profileListResponse.json');
         
-    //     $this->mock(new PlivoResponse($request,202, $body));
+        $this->mock(new PlivoResponse($request,202, $body));
         
-    //     $actual = $this->client->campaign->list();
+        $actual = $this->client->profile->list();
         
-    //     $this->assertRequest($request);
+        $this->assertRequest($request);
         
-    //     self::assertNotNull($actual);
+        self::assertNotNull($actual);
     }
 
+    function testProfileDelete()
+    {
+        $request = new PlivoRequest(
+            'DELETE',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Profile/06ecae31-4bf8-40b9-ac62-e902418e9935/', []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/profileDeleteResponse.json');
+
+        $this->mock(new PlivoResponse($request,200, $body));
+
+        $actual = $this->client->profile->delete('06ecae31-4bf8-40b9-ac62-e902418e9935');
+
+        $this->assertRequest($request);
+
+        self::assertNotNull($actual);
+    }
 }
