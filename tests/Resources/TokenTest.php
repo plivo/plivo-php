1c9d5c0d7c15ad82d6544e54a549fd15d11f3c13
kowshiksiva5 <kowshik.siva@students.iiit.ac.in>
2022-08-02 05:53:43 +0530

    Added version change

--- tests/Resources/TokenTest.php
+++ tests/Resources/TokenTest.php
@@ -35,6 +35,6 @@ class TokenTest extends BaseTestCase
         self::assertNotNull($actual);
 
 //        self::assertEquals($actual->token, "eyJhbGciOiJIUzI1NiIsImN0eSI6InBsaXZvO3Y9MSIsInR5cCI6IkpXVCJ9.eyJhcHAiOiIiLCJleHAiOjE2NTg3ODU4ODUsImlzcyI6Ik1BTURWTFpKWTJaR1k1TVdVMVpKIiwibmJmIjoxNjU4Njk5NDg1LCJwZXIiOnsidm9pY2UiOnsiaW5jb21pbmdfYWxsb3ciOmZhbHNlLCJvdXRnb2luZ19hbGxvdyI6ZmFsc2V9fSwic3ViIjoiS293c2hpayJ9.iWwtfH9QNO7nIE_HK0GSJ3U81oRQR9gcUScrPixBK_s");
-//        self::assertEquals($actual->apiId, "c3a4d6c0-0b9a-11ed-9fa2-0242ac110004");
+        self::assertEquals($actual->apiId, "c3a4d6c0-0b9a-11ed-9fa2-0242ac110004");
     }
 }
