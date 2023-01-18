8c436283b7445a97191fd78f637e8de7e4370bf5
kanishka2104 <88705700+kanishka2104@users.noreply.github.com>
2023-01-18 15:21:38 +0530

    version update

--- src/Plivo/Version.php
+++ src/Plivo/Version.php
@@ -20,7 +20,7 @@ class Version
     /**
      * @const int PHP helper library minor version number
      */
-    const MINOR = 37;
+    const MINOR = 40;
 
     /**
      * @const int PHP helper library patch number
