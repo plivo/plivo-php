e6f2fa122ebb260d6d45c114e592e94e45dc6b27
anindya-plivo <anindya.mahajan@plivo.com>
2023-06-28 16:01:43 +0530

    updated Version.php

--- src/Plivo/Version.php
+++ src/Plivo/Version.php
@@ -20,12 +20,12 @@ class Version
     /**
      * @const int PHP helper library minor version number
      */
-    const MINOR = 50;
+    const MINOR = 51;
 
     /**
      * @const int PHP helper library patch number
      */
-    const PATCH = 1;
+    const PATCH = 0;
     /**
      * @return string
      */
