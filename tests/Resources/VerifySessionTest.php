34c98ffc2407eabacece8bd9037d34532f839179
Rajneesh Katkam <rajneesh.katkam@plivo.com>
2023-08-03 02:00:47 +0530

    Testcases added for verify APIs

--- tests/Resources/VerifySessionTest.php
+++ tests/Resources/VerifySessionTest.php
@@ -13,146 +13,151 @@ use Plivo\Tests\BaseTestCase;
  */
 class VerifySessionTest extends BaseTestCase {
 
-    // public function testMessageCreateWithoutSrcPowerpackException()
-    // {
-    //     $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageSendResponse.json');
-
-    //     $this->mock(new PlivoResponse(new PlivoRequest(),200, $body));
-
-    //     $this->client->messages->create(null, ["+919012345678"], "Test", [], null);
-
-    // }
-
-    // public function testMessageCreateWithSrcPowerpackException()
-    // {
-    //     $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageSendResponse.json');
-
-    //     $this->mock(new PlivoResponse(new PlivoRequest(),200, $body));
-
-    //     $this->client->messages->create("+919999999999", ["+919012345678"], "Test", [], "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-
-    // }
 
     public function testVerifySessionCreate()
     {
         $request = new PlivoRequest(
             'POST',
-            'Account/MAXXXXXXXXXXXXXXXXXX/Message/',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/',
             [
                 "recipient" => "+919999999999"
             ]);
-        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionSendResponse.json');
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionCreateResponse.json');
 
         $this->mock(new PlivoResponse($request,202, $body));
-
         $actual = $this->client->verifySessions->create("+919999999999");
-
         self::assertNotNull($actual);
     }
 
-    // public function testnewMessageCreate()
-    // {
-    //     $request = new PlivoRequest(
-    //         'POST',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/',
-    //         [
-    //             "dst" => "+919012345678",
-    //             "text" => "Test",
-    //             "src" => "+919999999999"
-    //         ]);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageSendResponse.json');
+    public function testVerifySessionCreateWithRecipientException()
+    {
+        $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
 
-    //     $this->mock(new PlivoResponse($request,200, $body));
+        $request = new PlivoRequest(
+            'POST',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/',
+            [
+                "recipient" => "+919999999999"
+            ]);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionCreateResponse.json');
 
-    //     $actual = $this->client->messages->create([ "src" => "+919999999999", "dst" => "+919012345678", "text"  =>"Test"]);
+        $this->mock(new PlivoResponse($request,202, $body));
+        $actual = $this->client->verifySessions->create("");
 
-    //     self::assertNotNull($actual);
-    // }
+    }
 
-    // public function testMessageGet()
-    // {
-    //     $messageUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
-    //     $requesterIP = "192.168.1.1";
-    //     $request = new PlivoRequest(
-    //         'GET',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/'.$messageUuid.'/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageGetResponse.json');
+    public function testVerifySessionValidateWithSessionUUIDException()
+    {
+        $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
 
-    //     $this->mock(new PlivoResponse($request,200, $body));
+        $sessionUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
+        $otp = "999999";
+        $request = new PlivoRequest(
+            'POST',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/'.$sessionUuid.'/',
+            [
+                "otp" => "999999"
+            ]);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionValidateResponse.json');
 
-    //     $actual = $this->client->messages->get($messageUuid);
+        $this->mock(new PlivoResponse($request,200, $body));
+        $actual = $this->client->verifySessions->validate("", $otp);
 
-    //     $this->assertRequest($request);
+    }
 
-    //     self::assertNotNull($actual);
+    public function testVerifySessionValidateWithOTPException()
+    {
+        $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
 
-    //     self::assertEquals($actual->messageUuid, $messageUuid);
-    //     self::assertEquals($actual->requesterIP, $requesterIP);
-    // }
+        $sessionUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
+        $otp = "999999";
+        $request = new PlivoRequest(
+            'POST',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/'.$sessionUuid.'/',
+            [
+                "otp" => "999999"
+            ]);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionValidateResponse.json');
 
-    // public function testMessageGetwithPowerpack()
-    // {
-    //     $messageUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
-    //     $expected_ppk = "15c01cc2-4b9f-4d3b-bd15-3c4b38984cc4";
-    //     $request = new PlivoRequest(
-    //         'GET',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/'.$messageUuid.'/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageGetResponse.json');
+        $this->mock(new PlivoResponse($request,200, $body));
+        $actual = $this->client->verifySessions->validate($sessionUuid, "");
 
-    //     $this->mock(new PlivoResponse($request,200, $body));
+    }
 
-    //     $actual = $this->client->messages->get($messageUuid);
 
-    //     self::assertEquals($actual->powerpackID, $expected_ppk);
-    // }
-    // public function testMediaList()
-    // {
-    //     $messageUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
-    //     $request = new PlivoRequest(
-    //         'GET',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/'.$messageUuid.'/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageGetResponse.json');
 
-    //     $this->mock(new PlivoResponse($request,200, $body));
+    public function testVerifySessionValidate()
+    {
+        $sessionUuid = "5b40a428-bfc7-4daf-9d06-726c558bf3b8";
+        $otp = "999999";
+        $request = new PlivoRequest(
+            'POST',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/'.$sessionUuid.'/',
+            [
+                "otp" => "999999"
+            ]);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionValidateResponse.json');
 
-    //     $actual = $this->client->messages->get($messageUuid);
-    //     $request = new PlivoRequest(
-    //         'GET',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/'.$messageUuid.'/Media/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/mediaListResponse.json');
+        $this->mock(new PlivoResponse($request,200, $body));
 
-    //     $this->mock(new PlivoResponse($request,200, $body));
+        $actual = $this->client->verifySessions->validate($sessionUuid, $otp);
+        self::assertNotNull($actual);
+        self::assertEquals($actual->message, "session validated successfully.");
 
-    //     $mediaList = $actual->listMedia();
-    //     self::assertNotNull($mediaList);
+    }
 
-    // }
-    
-    // function testMessageList()
-    // {
-    //     $requesterIP1 = "192.168.1.1";
-    //     $requesterIP2 = "192.168.1.20";
-    //     $request = new PlivoRequest(
-    //         'Get',
-    //         'Account/MAXXXXXXXXXXXXXXXXXX/Message/',
-    //         []);
-    //     $body = file_get_contents(__DIR__ . '/../Mocks/messageListResponse.json');
+    public function testVerifySessionGet()
+    {
+        $sessionUuid = "4124e518-a8c9-4feb-8cff-d86636ba9234";
+        $requesterIP = "172.167.8.2";
+        $request = new PlivoRequest(
+            'GET',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/'.$sessionUuid.'/',
+            []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionGetResponse.json');
+
+        $this->mock(new PlivoResponse($request,200, $body));
+
+        $actual = $this->client->verifySessions->get($sessionUuid);
+        $this->assertRequest($request);
+        self::assertNotNull($actual);
+        self::assertEquals($actual->sessionUuid, $sessionUuid);
+        self::assertEquals($actual->requesterIP, $requesterIP);
+    }
+
+    public function testVerifySessionGetSessionUUIDException()
+    {
+        $this->expectPlivoException('Plivo\Exceptions\PlivoValidationException');
+
+        $sessionUuid = "4124e518-a8c9-4feb-8cff-d86636ba9234";
+        $request = new PlivoRequest(
+            'GET',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/'.$sessionUuid.'/',
+            []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionGetResponse.json');
+
+        $this->mock(new PlivoResponse($request,200, $body));
+        $this->client->verifySessions->get("");
         
-    //     $this->mock(new PlivoResponse($request,202, $body));
+    }
+
+    function testVerifySessionList()
+    {
+        $requesterIP1 = "110.226.182.196";
+        $requesterIP2 = "110.226.182.196";
+        $request = new PlivoRequest(
+            'Get',
+            'Account/MAXXXXXXXXXXXXXXXXXX/Verify/Session/',
+            []);
+        $body = file_get_contents(__DIR__ . '/../Mocks/verifySessionListResponse.json');
         
-    //     $actual = $this->client->messages->list;
+        $this->mock(new PlivoResponse($request,200, $body));
         
-    //     $this->assertRequest($request);
-        
-    //     self::assertNotNull($actual);
-    //     self::assertEquals($actual->resources[0]->requesterIP, $requesterIP1);
-    //     self::assertEquals($actual->resources[19]->requesterIP, $requesterIP2);
-    // }
+        $actual = $this->client->verifySessions->list();
+        $this->assertRequest($request);
+        self::assertNotNull($actual);
+        self::assertEquals($actual->resources[0]->requesterIP, $requesterIP1);
+        self::assertEquals($actual->resources[3]->requesterIP, $requesterIP2);
+    }
 
 }
