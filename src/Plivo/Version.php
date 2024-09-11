0fc196f24c86ad5f17176f8a6fa2025e8e0f214d
abinaya-plivo <abinaya.s@plivo.com>
2024-09-11 14:02:56 +0530

    version changed

--- src/Plivo/Version.php
+++ src/Plivo/Version.php
@@ -20,13 +20,13 @@ class Version
     /**
      * @const int PHP helper library minor version number
      */
-    const MINOR = 66;
+    const MINOR = 65;
 
     /**
      * @const int PHP helper library patch number
      */
 
-    const PATCH = 3;
+    const PATCH = 4;
 
     /**
      * @return string
